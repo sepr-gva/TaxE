6ae63dabf97b23a69b8062554bd604c40f604423
Michael Dipper <md859@york.ac.uk>
2014-11-25 15:39:40 +0000

    Added GDX project files

---
+++ android/src/com/seprgva/taxe/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.seprgva.taxe.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.seprgva.taxe.TaxE;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TaxE(), config);
+	}
+}
