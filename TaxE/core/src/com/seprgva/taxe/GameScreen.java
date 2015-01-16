3bf7466b81a4af845d4134f5c3df6ed1026e160b
Michael Dipper <md859@york.ac.uk>
2015-01-16 13:45:48 +0000

    Attempt 2. Manually corrections in gameScreen

--- TaxE/core/src/com/seprgva/taxe/GameScreen.java
+++ TaxE/core/src/com/seprgva/taxe/GameScreen.java
@@ -5,12 +5,21 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Array;
 
 import java.util.ArrayList;
 
 public class GameScreen implements Screen {
 	
 	final TaxE game;
+	private Texture greenSquare;
+	private Texture greySquare;
+	//private Rectangle tile;
+	
+	private SpriteBatch tileBatch;
 	
 	OrthographicCamera camera;
 	Map gameMap;
@@ -24,7 +33,8 @@ public class GameScreen implements Screen {
 		trainList.add(newTrain);
 		trainID++;
 	}
-	
+
+	//equivalent of create() method
 	public GameScreen(final TaxE gameInstance) {
 		game = gameInstance;
 		
@@ -33,9 +43,22 @@ public class GameScreen implements Screen {
 		
 		System.out.println(game.player1.companyName);
 		System.out.println(game.player2.companyName);
+		tileBatch = new SpriteBatch();
+		
+		//load some textures
+		greenSquare = new Texture(Gdx.files.internal("gameGraphics/greenSquare.png"));
+		greySquare = new Texture(Gdx.files.internal("gameGraphics/greySquare.png"));
 		
 		//Map is currently set to 20x20
 		gameMap = new Map();
+		
+		/*let's attempt to draw some blank tiles
+		tile = new Rectangle();
+		tile.x = 16;
+		tile.y = 16;
+		tile.width = 32;
+		tile.height = 32;
+		*/
 		
 		//Placing two cities on the map
 		//For now, this has to be done by replacing the appropriate blank tiles with city tiles
@@ -58,14 +81,40 @@ public class GameScreen implements Screen {
 		
 		camera.update();
 		//Make sure the coordinate systems match
+		
+		
+		tileBatch.setProjectionMatrix(camera.combined);
+		tileBatch.begin();
+		
+		//for(Rectangle tile: tiles) {
+			
+		for(int y = 0; y < gameMap.ySize; y++){
+			for(int x = 0; x <gameMap.xSize; x++){
+				Tile tile = gameMap.mapArray[x][y];
+				
+				if(tile.tileType == "rail"){
+						//Tile tile = gameMap.mapArray[x][y];
+						tileBatch.draw(greySquare, tile.x, tile.y);
+				}
+				else{
+					tileBatch.draw(greenSquare, tile.x, tile.y);
+				}
+			}
+		}
+			
+			
+		    //tileBatch.draw(greenSquare, tile.x, tile.y);
+		//}
+		tileBatch.end();
+		
+		
 		game.batch.setProjectionMatrix(camera.combined);
 		
 		//Draw
 		game.batch.begin();
 		game.batch.draw(game.player1.avatar, 500, 500, 100, 100);
 		game.batch.draw(game.player2.avatar, 600, 500, 100, 100);
-		game.font.draw(game.batch, "Player 1 name:" + game.player1.companyName 
-				+ ", Player 2 naem:" + game.player2.companyName, 450, 400);
+		game.font.draw(game.batch, "Player 1 name:" + game.player1.companyName + ", Player 2 naem:" + game.player2.companyName, 450, 400);
 		game.font.draw(game.batch, "Implement game here", 150, 200);
 		
 		//Only for testing purposes
